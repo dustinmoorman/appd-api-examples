be25059642fb5a3ef1dd27f9db9fc6544bff7b8a
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-05-04 12:42:12 -0500

    adql query example with json

---
+++ curl/get-adql-query-json.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+API_KEY="d67cc104-6ac5-4a83-9cc1-cee1766e58a8" # dmoorman-test-api-key
+GLOBAL_ACCOUNT_NAME="customer1_0cf8fe6a-74e0-4dac-b739-8e36b2d8e2ec"
+CONTROLLER_HOST="http://dmoormandevelopmentlab-kfk90btr.appd-sales.com:9080"
+
+curl -X POST -d "SELECT * FROM transactions WHERE userExperience = ERROR" -H"X-Events-API-AccountName:${GLOBAL_ACCOUNT_NAME}" -H"X-Events-API-Key:${API_KEY}" -H"Content-type: application/vnd.appd.events+json;v=2" "${CONTROLLER_HOST}/events/query?limit=5"
