2a33850ec020ac356737cb613dafd352bd8a4ea9
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-05-05 16:53:19 -0500

    add gitignore and rename to jq data extraction...

--- curl/jq-data-extraction-web-session-events.sh
+++ curl/jq-data-extraction-web-session-events.sh
@@ -3,6 +3,7 @@
 API_KEY="d67cc104-6ac5-4a83-9cc1-cee1766e58a8" # dmoorman-test-api-key
 GLOBAL_ACCOUNT_NAME="customer1_0cf8fe6a-74e0-4dac-b739-8e36b2d8e2ec"
 CONTROLLER_HOST="http://dmoormandevelopmentlab-kfk90btr.appd-sales.com:9080"
+LIMIT=2
 
 ADQL_QUERY="SELECT * FROM web_session_records" 
 
@@ -10,10 +11,16 @@ curl -s -X POST -d "${ADQL_QUERY}" \
   -H"X-Events-API-AccountName:${GLOBAL_ACCOUNT_NAME}" \
   -H"X-Events-API-Key:${API_KEY}" \
   -H"Content-type: application/vnd.appd.events+json;v=2" \
-  "${CONTROLLER_HOST}/events/query?limit=5" \
+  "${CONTROLLER_HOST}/events/query?limit=${LIMIT}" \
   -o .query_results
 
 TOTAL_ROWS=$(cat .query_results | jq -r .[].total)
+COUNT_RETURNED=$(cat .query_results | jq '.[].results | length')
+
+# Get last element of array, and find the timestamp
+cat .query_results | jq '.[].results | .[-1]'
 
 echo "Total Result Rows: ${TOTAL_ROWS}";
+echo "Returned in query: ${COUNT_RETURNED}";
+
 rm .query_results
