e67f58fd250c43ef5f603eaac766b3393834d460
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-05-06 07:05:44 -0500

    spacing fixes

--- curl/oauth-metric-sample-json.sh
+++ curl/oauth-metric-sample-json.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Set initial user info & controller host
@@ -9,11 +8,15 @@ CONTROLLER_HOST="http://dmoormandevelopmentlab-kfk90btr.appd-sales.com:8090"
 DURATION_MINS=60
 
 # Obtain Access Token with Client Secret
-curl -s -X POST -H "Content-Type: application/vnd.appd.cntrl+protobuf;v=1" "${CONTROLLER_HOST}/controller/api/oauth/access_token" -d "grant_type=client_credentials&client_id=${OAUTH_CLIENT_NAME}@${ACCOUNT_NAME}&client_secret=${OAUTH_CLIENT_SECRET}" -o .access_token_payload
+curl -s -X POST -H "Content-Type: application/vnd.appd.cntrl+protobuf;v=1" \
+  "${CONTROLLER_HOST}/controller/api/oauth/access_token" \
+  -d "grant_type=client_credentials&client_id=${OAUTH_CLIENT_NAME}@${ACCOUNT_NAME}&client_secret=${OAUTH_CLIENT_SECRET}" \
+  -o .access_token_payload
 
 ACCESS_TOKEN=$(cat .access_token_payload | jq -r .access_token)
 
 # Get all applications in XML form
-curl -H "Authorization:Bearer ${ACCESS_TOKEN}" "${CONTROLLER_HOST}/controller/rest/applications/AppDynamics%20Analytics-2/metric-data?metric-path=Analytics%7CInstrumentable%20Tomcat%20Call%20Errors&time-range-type=BEFORE_NOW&duration-in-mins=${DURATION_MINS}"
+curl -H "Authorization:Bearer ${ACCESS_TOKEN}" \
+  "${CONTROLLER_HOST}/controller/rest/applications/AppDynamics%20Analytics-2/metric-data?metric-path=Analytics%7CInstrumentable%20Tomcat%20Call%20Errors&time-range-type=BEFORE_NOW&duration-in-mins=${DURATION_MINS}"
 
 rm .access_token_payload
