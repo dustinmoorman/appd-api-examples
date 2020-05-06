c27cc2235ed06fd1ae0fdec18bd3402f0a06e31e
Dustin Moorman <dustin.moorman@appdynamics.com>
2020-05-06 07:02:39 -0500

    cleanup

--- curl/get-applications-xml.sh
+++ curl/get-applications-xml.sh
@@ -7,4 +7,5 @@ ACCOUNT_NAME="customer1"
 CONTROLLER_HOST="http://dmoormandevelopmentlab-kfk90btr.appd-sales.com:8090"
 
 # Get all applications in XML form
-curl --user ${USER_NAME}@${ACCOUNT_NAME}:${USER_PASS} ${CONTROLLER_HOST}/controller/rest/applications
+curl --user "${USER_NAME}@${ACCOUNT_NAME}:${USER_PASS}" \
+  "${CONTROLLER_HOST}/controller/rest/applications"
